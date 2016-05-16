86da258b4da61683dc71d1dd5d34cfb534deaa79
XLuoChen <1425145741@qq.com>
2016-05-16 21:17:19 +0800

    add separators.js

---
+++ src/separators.js
@@ -0,0 +1,19 @@
+'use strict';
+
+function thousands_separators(num) {
+    var numToString = num.toString();
+    var splitString = numToString.split('.');
+    var separatedNum = '';
+
+    if(splitString.length === 1){
+        separatedNum += splitString[0].replace(/(\d)(?=(?:\d\d\d)+(?!\d))/g,"$1,");
+    }
+    else{
+        separatedNum += splitString[0].replace(/(\d)(?=(?:\d\d\d)+(?!\d))/g,"$1,");
+        separatedNum += '.' + splitString[1];
+    }
+
+    return separatedNum;
+}
+
+module.exports = thousands_separators;
